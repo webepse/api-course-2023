c73b617a8561c6c86c87c90be912d7ca7f8f4039
berti <berti@epse.be>
2023-01-30 09:46:25 +0100

    cours 6 - authentifcation avec JSON JWT Token

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -26,11 +26,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['users_read','customers_read','invoices_read'])]
+    #[Groups(['users_read','customers_read','invoices_read','invoices_subresource'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    #[Groups(['users_read','customers_read','invoices_read'])]
+    #[Groups(['users_read','customers_read','invoices_read','invoices_subresource'])]
     #[Assert\NotBlank(message: "L'adresse E-mail ne peut pas être vide")]
     #[Assert\Email(message:"Le format de l'adresse E-ail doit être valide")]
     private ?string $email = null;
@@ -47,13 +47,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['users_read','customers_read','invoices_read'])]
+    #[Groups(['users_read','customers_read','invoices_read','invoices_subresource'])]
     #[Assert\NotBlank(message: "Le prénom ne peut pas être vide")]
     #[Assert\Length(min:2, minMessage: "Le prénom doit faire entre 2 et 255 caractères", max: 255, maxMessage: "le prénom doit faire entre 2 et 255 caractères")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['users_read','customers_read','invoices_read'])]
+    #[Groups(['users_read','customers_read','invoices_read','invoices_subresource'])]
     #[Assert\NotBlank(message: "Le nom ne peut pas être vide")]
     #[Assert\Length(min:2, minMessage: "Le nom doit faire entre 2 et 255 caractères", max: 255, maxMessage: "le nom doit faire entre 2 et 255 caractères")]
     private ?string $lastName = null;
